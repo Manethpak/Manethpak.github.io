67f8a8d67ff7b01dca566e91d07bed64bffe6d8e
Maneth Pak <manethpak00@gmail.com>
2022-06-27 18:08:29 +0700

    reconfig tailwind and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        100: "#424549",
+        200: "#36393e",
+        300: "#282b30",
+        400: "#1e2124",
+      },
+      white: colors.white,
+      gray: colors.gray,
+      violet: colors.violet,
+      indigo: colors.indigo,
+    },
   },
   plugins: [],
 };
