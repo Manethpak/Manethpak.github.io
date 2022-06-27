4dceb37f738eb59d13dc4f0f9f8a67718f4ab9ab
Maneth Pak <manethpak00@gmail.com>
2022-06-27 18:06:01 +0700

    adding dark mode

---
+++ src/util/useDarkmode.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { Theme } from "../constant/theme.enum";
+
+export default function useDarkmode() {
+  const [theme, setTheme] = useState<Theme>(
+    localStorage.getItem("theme") as Theme
+  );
+  const switchTheme = () => {
+    setTheme(theme === Theme.Light ? Theme.Dark : Theme.Light);
+  };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(theme === Theme.Light ? Theme.Dark : Theme.Light);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return { switchTheme };
+}
