5c0bc9e2a455552601e7d74bbfdd12418a2edd74
Maneth Pak <manethpak00@gmail.com>
2022-06-28 19:44:16 +0700

    add sun and moon icon for theme

---
+++ src/components/icons/MoonIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { GenericProps } from "src/constant/props.type";
+
+const Moon = ({ className }: GenericProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  );
+};
+
+export default Moon;
