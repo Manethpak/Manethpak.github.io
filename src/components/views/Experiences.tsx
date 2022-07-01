24a636adff7e663b6d21ca6fb6b1315a3cefc1c7
Maneth Pak <manethpak00@gmail.com>
2022-07-01 18:03:41 +0700

    add view components

---
+++ src/components/views/Experiences.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Experiences = () => {
+  return (
+    <section
+      id="experiences"
+      className="h-screen w-full flex justify-center items-start snap-always snap-center"
+    >
+      a
+    </section>
+  );
+};
+
+export default Experiences;
---
+++ src/components/views/IndexView.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const IndexView = () => {
+  return (
+    <section
+      id="index"
+      className="h-screen w-full flex justify-center items-start snap-always snap-center"
+    >
+      <div className="h-40 w-[30rem] shadow-lg bg-primary-100 dark:bg-primary-400 rounded-3xl p-8 mt-20">
+        <h1 className="text-3xl font-semibold">Hi, I'm Maneth.</h1>
+      </div>
+    </section>
+  );
+};
+
+export default IndexView;
