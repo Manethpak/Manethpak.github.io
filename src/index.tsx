ad7643b9e388a761c7b75e62c694809b2fe8f2c8
Maneth Pak <manethpak00@gmail.com>
2022-07-01 18:10:07 +0700

    refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import HomePage from "./views/HomePage";
+import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <HomePage />
-  </React.StrictMode>
+  </React.Fragment>
 );
